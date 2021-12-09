7968bda9f32eb67ad6b9b12f374a49a8550656bf
Latrell <lll4xy@virginia.edu>
2021-12-09 11:18:00 -0500

    exercise 7.8

--- src/App.js
+++ src/App.js
@@ -19,15 +19,19 @@ const useField = (type) => {
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
+  useEffect(() => {
+    axios.get(baseUrl)
+    .then(response => setResources(response.data))
+  }, [baseUrl])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
-    create
+    create,
   }
 
   return [
